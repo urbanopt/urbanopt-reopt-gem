3c06e160be56fa2f5d006ba8a7987126d20ea21e
TK-23 <tedkwasnik@gmail.com>
2020-03-24 15:46:46 -0600

    Update feature_report_adapter.rb

--- lib/urbanopt/reopt/feature_report_adapter.rb
+++ lib/urbanopt/reopt/feature_report_adapter.rb
@@ -105,7 +105,7 @@ module URBANopt # :nodoc:
           col_num = feature_report.timeseries_csv.column_names.index('Electricity:Facility')
           t = CSV.read(feature_report.timeseries_csv.path, headers: true, converters: :numeric)
           energy_timeseries_kwh = t.by_col[col_num].map { |e| ((e || 0) * 0.293071) } # convert kBTU to KWH
-          if energy_timeseries_kwh.length < feature_report.timesteps_per_hour * 8760
+          if energy_timeseries_kwh.length < (feature_report.timesteps_per_hour * 8760)
             start_date = Time.parse(t.by_col["Datetime"][0])
             start_ts = (((start_date.yday * 60.0 * 60.0 * 24) + (start_date.hour * 60.0 * 60.0) + (start_date.min * 60.0) + start_date.sec) /
                         (( 60 / feature_report.timesteps_per_hour ) * 60)).to_int
@@ -114,7 +114,7 @@ module URBANopt # :nodoc:
                         (( 60 / feature_report.timesteps_per_hour ) * 60)).to_int
             energy_timeseries_kwh = [0.0]*(start_ts-1) + energy_timeseries_kwh + [0.0]*((feature_report.timesteps_per_hour * 8760) - end_ts)
           end
-          reopt_inputs[:Scenario][:Site][:LoadProfile][:loads_kw] = energy_timeseries_kwh.map { |e| e ? e : 0 }[0,8760]
+          reopt_inputs[:Scenario][:Site][:LoadProfile][:loads_kw] = energy_timeseries_kwh.map { |e| e ? e : 0 }[0,(feature_report.timesteps_per_hour * 8760)]
         rescue StandardError
           @@logger.error("Could not parse the annual electric load from the timeseries csv - #{feature_report.timeseries_csv.path}")
           raise "Could not parse the annual electric load from the timeseries csv - #{feature_report.timeseries_csv.path}"
