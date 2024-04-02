bb8235401a636f2f7a1aa37fd1736f1304a56537
Nathan Moore <nathan.moore@nrel.gov>
2024-04-02 08:43:56 -0600

    update one tests dummy_data to conform to v3 specification

--- spec/tests/urbanopt_reopt_spec.rb
+++ spec/tests/urbanopt_reopt_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe URBANopt::REopt do
 
   it 'can connect to reopt' do
     api = URBANopt::REopt::REoptLiteAPI.new(DEVELOPER_NREL_KEY, false)
-    dummy_data = { Scenario: { Site: { latitude: 40, longitude: -110, Wind: { max_kw: 0 }, ElectricTariff: { urdb_label: '594976725457a37b1175d089' }, LoadProfile: { doe_reference_name: 'Hospital', annual_kwh: 1000000 } } } }
+    dummy_data = { Site: { latitude: 40, longitude: -110}, Wind: { max_kw: 0 }, ElectricTariff: { urdb_label: '594976725457a37b1175d089' }, ElectricLoad: { doe_reference_name: 'Hospital', annual_kwh: 1000000 } }
     ok = api.check_connection(dummy_data)
     expect(ok).to be true
   end
