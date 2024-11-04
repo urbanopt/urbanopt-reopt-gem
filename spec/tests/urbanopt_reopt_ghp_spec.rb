a5b3c6e875534707e2e0c301ec0e7319610fe55b
Nathan Moore <nathan.moore@nrel.gov>
2024-11-04 15:30:11 -0700

    fix typos and linting

--- spec/tests/urbanopt_reopt_ghp_spec.rb
+++ spec/tests/urbanopt_reopt_ghp_spec.rb
@@ -8,22 +8,25 @@ require 'json-schema'
 
 
 RSpec.describe URBANopt::REopt do
-    
     run_dir = Pathname(__FILE__).dirname.parent / 'files' / 'run' / 'baseline_scenario_ghe'
-    reopt_input_dir = File.join(run_dir, 'reopt_ghp', 'reopt_ghp_inputs')
-    reopt_ghp_output = File.join(run_dir, 'reopt_ghp', 'reopt_ghp_outputs')
+    spec_files_dir = Pathname(__FILE__).dirname.parent / 'files'
+    source_lib = Pathname(__FILE__).dirname.parent.parent / 'lib'
 
-    modelica_result = Pathname(__FILE__).dirname.parent / 'files' / 'modelica_4'
-    system_parameter =  Pathname(__FILE__).dirname.parent / 'files' / 'system_parameter_1.json'
-    reopt_ghp_assumption = File.join(Pathname.new(__FILE__).dirname.parent.parent, 'lib', 'urbanopt', 'reopt', 'reopt_ghp_files', 'reopt_ghp_assumption.json')
-    schema_path = File.join(Pathname.new(__FILE__).dirname.parent.parent, 'lib', 'urbanopt', 'reopt', 'reopt_schema', 'REopt-GHP-input.json')
+    modelica_result = spec_files_dir / 'modelica_4'
+    system_parameter =  spec_files_dir / 'system_parameter_1.json'
+    reopt_ghp_assumption = source_lib / 'urbanopt' / 'reopt' / 'reopt_ghp_files' / 'reopt_ghp_assumption.json'
+    schema_path = source_lib / 'urbanopt' / 'reopt' / 'reopt_schema' / 'REopt-GHP-input.json'
     schema =  JSON.parse(File.read(schema_path))
     nrel_developer_key = DEVELOPER_NREL_KEY
+    reopt_input_dir = run_dir / 'reopt_ghp' / 'reopt_ghp_inputs'
+    reopt_ghp_output = run_dir / 'reopt_ghp' / 'reopt_ghp_outputs'
+
+    schema =  source_lib / 'urbanopt' / 'reopt' / 'reopt_schema' / 'REopt-GHP-input.json'
 
     @run_id = nil
     before(:all) do
         # Load the JSON data before running the tests
-        @building_4_path = File.join(reopt_input_dir, 'GHP_building_4.json')        
+        @building_4_path = File.join(reopt_input_dir, 'GHP_building_4.json')
         @building_5_path = File.join(reopt_input_dir, 'GHP_building_5.json')
         @ghp_path = File.join(reopt_input_dir, 'GHX_7932a208-dcb6-4d23-a46f-288896eaa1bc.json')
 
@@ -40,10 +43,10 @@ RSpec.describe URBANopt::REopt do
         # output folder exist
         expect(Dir.exist?(File.join(reopt_input_dir)))
 
-        # expect building file exisits
+        # expect building file exists
         expect(File.exist?(File.join(reopt_input_dir, 'GHP_building_4.json')))
         expect(File.exist?(File.join(reopt_input_dir, 'GHP_building_5.json')))
-        
+
         # expect ghp file exists
         expect(File.exist?(File.join(reopt_input_dir, 'GHX_7932a208-dcb6-4d23-a46f-288896eaa1bc.json')))
     end
@@ -71,20 +74,20 @@ RSpec.describe URBANopt::REopt do
         expect(building_4_data[:SpaceHeatingLoad][:fuel_loads_mmbtu_per_hour]).to_not be_empty
         expect(building_4_data[:SpaceHeatingLoad][:fuel_loads_mmbtu_per_hour].size).to eq(8760)
         expect(building_4_data[:ElectricTariff][:urdb_label]).to_not be_nil
-        
+
         ghp_data = JSON.parse(File.read(@ghp_path), symbolize_names: true)
         expect(ghp_data[:GHP][:ghpghx_responses][0][:outputs][:yearly_ghx_pump_electric_consumption_series_kw]).to_not be_empty
         expect(ghp_data[:GHP][:ghpghx_responses][0][:outputs][:yearly_ghx_pump_electric_consumption_series_kw].size).to eq(8760)
     end
 
     it 'can connect to the REopt API and generate UUID' do
-        
+
         reopt_input_file_path = File.join(reopt_input_dir, 'GHP_building_4.json')
         reopt_input_data = nil
         File.open(reopt_input_file_path, 'r') do |f|
             reopt_input_data = JSON.parse(f.read)
         end
-        post_url = "https://developer.nrel.gov/api/reopt/stable/job/?api_key=#{nrel_developer_key}"
+        post_url = "https://developer.nrel.gov/api/reopt/stable/job/?api_key=#{DEVELOPER_NREL_KEY}"
 
         # Parse the URL and prepare the HTTP request
         uri = URI.parse(post_url)
@@ -100,7 +103,7 @@ RSpec.describe URBANopt::REopt do
         end
 
 
-        expect(response).to be_a(Net::HTTPSuccess) 
+        expect(response).to be_a(Net::HTTPSuccess)
         run_id_dict = JSON.parse(response.body)
         @run_id = run_id_dict['run_uuid']
 
@@ -112,7 +115,7 @@ RSpec.describe URBANopt::REopt do
         Dir.foreach(reopt_ghp_output) do |file|
             next if file == '.' || file == '..' # Skip current and parent directory references
             file_path = File.join(reopt_ghp_output, file)
-          
+
             if File.file?(file_path)
                 File.open(file_path, 'r') do |f|
                     file_data = JSON.parse(f.read, symbolize_names: true)
